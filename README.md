04daf9775af641e72c04ec399db77bfaa52d894c
kennergf <kenner.g.figueiredo@gmail.com>
2021-10-18 23:36:38 +0100

    Created script for SQL Server

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Stock.Companies
+
+# DataBase
+The Data project has the class "MSSQLContextFactory" that is used to execute Entity Framework Migrations, 
+to use it is necessary to configure the ConnectionString on the appsettings on the same project for SQL Server.
+Alternatively the SQL Script available at "StockCompany.sql" can be executed
---
+++ Stock.Companies.Data/StockCompany.sql
@@ -0,0 +1,34 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Company] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] varchar(256) NOT NULL,
+    [Exchange] varchar(256) NOT NULL,
+    [Ticker] varchar(256) NOT NULL,
+    [ISIN] varchar(12) NOT NULL,
+    [WebSite] varchar(256) NULL,
+    CONSTRAINT [PK_Company] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE UNIQUE INDEX [IX_Company_ISIN] ON [Company] ([ISIN]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20211017103657_Created Company', N'5.0.11');
+GO
+
+COMMIT;
+GO
+
